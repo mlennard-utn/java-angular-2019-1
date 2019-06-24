1c0953e76b5a212ce46fd495e9b4252c09d63a43
unknown <arielsilvainfo@gmail.com>
2019-06-24 20:55:34 -0300

    Layout for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [{
   path: 'signin', component: SigninComponent
+}, 
+{
+  path: 'menu', component: MenuComponent
+},
+{
+  path: 'footer', component: FooterComponent
+},
+{
+  path: 'layout', component: LayoutComponent
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,21 @@ import { SigningserviceService } from './signingservice.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule }   from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { LayoutComponent } from './layout/layout.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SigninComponent,
-    NewProductComponent
+    NewProductComponent,
+    LayoutComponent,
+    MenuComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
